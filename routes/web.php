9a817da7e2be15ccc9db706a2d71cd4217fccc83
yuya3333 <yuyanami33@gmail.com>
2020-12-25 22:26:24 +0900

    second commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('hello', function () {
+    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+});
